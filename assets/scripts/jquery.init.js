3d414bfba808ce97908b74147dc11de0f0ab3bb2
cs <bartus.csongor@gmail.com>
2014-11-01 10:45:40 +0200

    Fullpage set up

--- assets/scripts/jquery.init.js
+++ assets/scripts/jquery.init.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
   
+  $('#fullpage').fullpage({
+    navigation: true,
+    slidesNavigation: true,
+    scrollOverflow: true,
+    verticalCentered: false,
+    //css3: true, breaks navigation
+  });
 });
 
